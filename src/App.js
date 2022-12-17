5be1f27b688671129cd9b555e7547fafd2772199
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2022-12-17 11:07:46 +0700

    created searchbox

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import * as Page from "./pages";
-import { Navbar, Footer } from "./components";
+import { Footer, Navbar, SearchBox } from "./components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
-
+      <SearchBox />
       <Switch>
         <Route exact path="/">
           <Page.Home />
